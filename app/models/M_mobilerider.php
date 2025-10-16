c17e6472bc80849875cf390f395ca41b548d0c01
seemasiyam28@gmail.com <seemasiyam28@gmail.com>
2025-10-16 13:44:05 +0530

    RDFC-38 note-crud

--- app/models/M_mobilerider.php
+++ app/models/M_mobilerider.php
@@ -1,10 +1,49 @@
 <?php
-class M_mobilerider {
+class M_mobilerider
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new Database();
     }
 
-    
+    public function addNote($data)
+    {
+        $this->db->query("INSERT INTO notes ( userID, title, content, created_at) VALUES (:userID, :title, :content, NOW())");
+
+        // Bind values
+        $this->db->bind(":userID", $data['userID']);
+        $this->db->bind(":title", $data['title']);
+        $this->db->bind(":content", $data['content']);
+
+        // Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getAllNotes() {
+        $this->db->query("SELECT * FROM notes");
+        return $this->db->resultSet();
+    }
+
+   public function deleteNoteById($id) {
+    $this->db->query("DELETE FROM notes WHERE id = :id");
+    $this->db->bind(':id', $id);
+    return $this->db->execute();
+}
+
+public function updateNoteById($id, $title, $content) {
+    $this->db->query("UPDATE notes SET title = :title, content = :content WHERE id = :id");
+    $this->db->bind(":id", $id);        // Changed from :userID to :id
+    $this->db->bind(":title", $title);
+    $this->db->bind(":content", $content);
+    return $this->db->execute();
+}
+
+
+
 }
--- app/views/mobilerider/v_messages.php
+++ app/views/mobilerider/v_messages.php
@@ -1,14 +1,87 @@
 <?php require_once APP_ROOT . '/views/inc/components/header.php'; ?>
 
-  <?php require_once APP_ROOT . '/views/components/v_mobilerider_sidebar.php'; ?>
+<?php require_once APP_ROOT . '/views/components/v_mobilerider_sidebar.php'; ?>
 
 
-    <!-- Content will be loaded here -->
+<!-- Content will be loaded here -->
+<!-- Material Icons -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!-- Link to Dashboard CSS -->
+<link rel="stylesheet" href="<?php echo URL_ROOT; ?>/css/mobilerider/messages.css">
+
+<div class="messages-container">
+
+  <!-- Left: Chat list -->
+  <div class="chat-list-panel">
     
-    </main>
+    <input type="text" placeholder="Search" class="search">
+
+    <div class="chat-list">
+      <div class="chat-item">
+        <div class="avatar"></div>
+        <div class="chat-info">
+          <p class="name">Ashen Fernando <span class="role">Premise Officer</span></p>
+          <p class="preview">I was present on duty on 18th July...</p>
+        </div>
+        <span class="time">14:32</span>
+      </div>
+
+      <div class="chat-item">
+        <div class="avatar"></div>
+        <div class="chat-info">
+          <p class="name">John Silva <span class="role">Supervisor</span></p>
+          <p class="preview">Officer Ravindu Fernando was...</p>
+        </div>
+        <span class="time">12:32</span>
+      </div>
+
+      <div class="chat-item">
+        <div class="avatar"></div>
+        <div class="chat-info">
+          <p class="name">Naduni Senanayake <span class="role">HR Officer</span></p>
+          <p class="preview">Officer training certificates...</p>
+        </div>
+        <span class="time">01:42</span>
+      </div>
+
+      <div class="chat-item">
+        <div class="avatar"></div>
+        <div class="chat-info">
+          <p class="name">Nishadi Dissanayake <span class="role">Admin</span></p>
+          <p class="preview">Updated shift schedules for all...</p>
+        </div>
+        <span class="time">01:22</span>
+      </div>
+    </div>
+  </div>
+
+  <!-- Right: Chat messages -->
+  <div class="chat-window">
+    <div class="chat-header">Ashen Fernando</div>
+
+    <div class="messages">
+      <div class="msg sent">What do you mean?</div>
+      <div class="msg received">I think the idea that things are chaning isnt good</div>
+      <div class="msg sent">What do you mean?</div>
+      <div class="msg received">I think the idea that things are chaning isnt good</div>
+      <div class="msg sent">What do you mean?</div>
+      <div class="msg received">I think the idea that things are chaning isnt good</div>
     </div>
 
-    <div class="backdrop" id="backdrop" hidden></div>
+    <div class="chat-input">
+      <input type="text" placeholder="Type a message">
+      <button class="sent-btn">^</button>
+    </div>
+  </div>
 
-    <script src="<?php echo URL_ROOT; ?>/js/components/sidebar.js"></script>
+</div>
+
+
+</main>
+</div>
+
+<div class="backdrop" id="backdrop" hidden></div>
+
+<script src="<?php echo URL_ROOT; ?>/js/components/sidebar.js"></script>
 <?php require_once APP_ROOT . '/views/inc/components/footer.php'; ?>
