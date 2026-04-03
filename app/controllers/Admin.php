6dd6883e353a2da490db9343feecb5ff54308d39
Pasan Mihiranga <pmihirangaa321@gmail.com>
2026-04-03 17:57:19 +0530

    update(Pasan)

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -812,7 +812,7 @@ class Admin extends Controller {
         elseif($role == 'ct') $role_name = "Care Taker";
         
         // Get the logged-in admin ID (you need to adjust this based on your auth system)
-        $adminId = $_SESSION['user_userID'] ?? 1; // Default to 1 if session not set
+        $adminId = $_SESSION['user_id'] ?? 1; // Default to 1 if session not set
         
         $result = $this->adminModel->acceptOfficerApplication($id, $adminId, $role);
         
